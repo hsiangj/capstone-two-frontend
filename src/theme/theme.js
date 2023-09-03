060f5a5a10972c7c6eed0fa21d3939f473415f66
Jessica Hsiang <hsiang.jessica@gmail.com>
2023-09-03 15:38:15 -0700

    Set up MUI theme provider and add customized favicon

---
+++ src/theme/theme.js
@@ -0,0 +1,15 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#045681', // Customize the primary color
+    },
+    secondary: {
+      main: '#98bccc', // Customize the secondary color
+    },
+  },
+});
+
+export default theme;
+
