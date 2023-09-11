666267bcce5bcad0b956b263ca1392837895abff
Jessica Hsiang <hsiang.jessica@gmail.com>
2023-09-11 14:13:22 -0700

    Update api base url

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_BASE_URL || "http://localhost:3001";
+const BASE_URL = "https://expensebud-backend.onrender.com" || "http://localhost:3001";
 
 class ExpenseBudApi {
   static token;
